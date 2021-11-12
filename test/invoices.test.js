79e7bce143537b60bcacc32f04cfd38850c47fd9
MaysharaF <mayshara@alunos.utfpr.edu.br>
2021-11-12 16:09:34 -0300

    Realizando teste 3 - falhando

--- test/invoices.test.js
+++ test/invoices.test.js
@@ -22,9 +22,9 @@ describe("Filter", () => {
   const client2 = new Client("Nokk", new Date(2021, 8, 11), "São Paulo");
   const client3 = new Client("Dokka", new Date(2021, 4, 10), "Pará");
 
-  const invoice1 = new Invoice(1, 1630, new Date(2021, 11, 3), client1);
-  const invoice2 = new Invoice(2, 1500, new Date(2021, 12, 2), client2);
-  const invoice3 = new Invoice(3, 2000, new Date(2021, 11, 18), client3);
+  const invoice1 = new Invoice(1, 5000, new Date(2021, 5, 3), client1);
+  const invoice2 = new Invoice(2, 4000, new Date(2021, 7, 2), client2);
+  const invoice3 = new Invoice(3, 2000, new Date(2021, 9, 18), client3);
 
   test("If the invoice amount is less than 2000", () => {
     const invoices = [invoice1, invoice2, invoice3];
@@ -36,10 +36,20 @@ describe("Filter", () => {
     expect(result).toEqual((filteredInvoices));
 
   }),
-    test("If the invoice amount is between 2000 and 2500 and the date is less than or equal to one month ago", () => {
+    test("If the invoice amount is between 2000 and 2500 and 1m", () => {
       const invoices = [invoice1, invoice2, invoice3];
 
       const filteredInvoices = [];
+
+      var result = invoicesFilter(invoices);
+
+      expect(result).toEqual((filteredInvoices));
+
+    }),
+    test("If the invoice amount is between 2500 and 3000 and 2m", () => {
+      const invoices = [invoice1, invoice2, invoice3];
+
+      const filteredInvoices = [invoice1, invoice2];
 
       var result = invoicesFilter(invoices);
 
