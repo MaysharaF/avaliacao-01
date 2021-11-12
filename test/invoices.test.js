cf46392e1a5b8e9c1c279350e2cd052880336be0
MaysharaF <mayshara@alunos.utfpr.edu.br>
2021-11-12 16:01:07 -0300

    Realizando teste 1 - falha

--- test/invoices.test.js
+++ test/invoices.test.js
@@ -24,11 +24,12 @@ describe("Filter", () => {
 
   const invoice1 = new Invoice(1, 1630, new Date(2021, 11, 3), client1);
   const invoice2 = new Invoice(2, 1500, new Date(2021, 12, 2), client2);
-  const invoice3 = new Invoice(3, 1980, new Date(2021, 12, 18), client3);
+  const invoice3 = new Invoice(3, 3000, new Date(2021, 12, 18), client3);
 
   test("If the invoice amount is less than 2000", () => {
     const invoices = [invoice1, invoice2, invoice3];
-    const filteredInvoices = [];
+
+    const filteredInvoices = [invoice3];
 
     var result = invoicesFilter(invoices);
 
